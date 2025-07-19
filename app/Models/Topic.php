6fdae0aff4c1dff46f72d18903d79faf86348101
tabcodes <tabcodes@tabcodes.dev>
2025-07-19 01:59:04 -0400

    Fix imports

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -2,6 +2,9 @@
 
 namespace App\Models;
 
+use App\Models\User;
+use App\Models\Board;
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
