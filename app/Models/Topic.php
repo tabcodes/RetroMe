68ff847961d6fa4c21c4db0a5df63c8f1c505b27
tabcodes <tabcodes@tabcodes.dev>
2025-07-19 01:57:39 -0400

    Fix topic relation methods

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Topic extends Model
 {
@@ -19,4 +20,31 @@ class Topic extends Model
         'content',
         'notes',
     ];
+
+    /**
+     * Summary of category
+     * @return BelongsTo<Category, Topic>
+     */
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    /**
+     * Summary of creator
+     * @return BelongsTo<User, Topic>
+     */
+    public function creator(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Summary of board
+     * @return BelongsTo<Board, Topic>
+     */
+    public function board(): BelongsTo
+    {
+        return $this->belongsTo(Board::class);
+    }
 }
