cebe563a4e073d18c556db8f54d5cb105cb98e73
tabcodes <tabcodes@tabcodes.dev>
2025-07-18 20:46:10 -0400

    Migration for Boards; skeleton for model and controller

---
+++ database/migrations/2025_07_12_032018_create_boards_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create(table: 'boards', callback: function (Blueprint $table) {
+            $table->ulid(column: 'id')->primary();
+            $table->string(column: 'name', length: 255);
+            $table->foreignId('creator_id')->references(column: 'id')->on(table: 'users');
+            
+
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table(table: 'boards', callback: function(Blueprint $table): void {
+            $table->dropForeign(index: ['creator_id']);
+        });
+
+        Schema::dropIfExists(table: 'boards');
+    }
+};
