4e2bc0e1aecf7b0f3d774ce9973459c8e4e25106
tabcodes <tabcodes@tabcodes.dev>
2025-09-01 19:02:50 -0400

    Add routes for board creation and index

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BoardController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -11,6 +12,9 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
+
+    Route::get('boards', [BoardController::class, 'index']);
+    Route::get('board/create', [BoardController::class, 'create']);
 });
 
 require __DIR__.'/settings.php';
